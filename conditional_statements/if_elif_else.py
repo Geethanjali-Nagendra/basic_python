4f750add4cbae2e266c21274385ab77ee976d6d4
Geethanjali <geethanjali.nagendra@gmail.com>
2025-06-14 19:18:14 +0200

    corrected print statement for if-else conditional statement

--- conditional_statements/if_elif_else.py
+++ conditional_statements/if_elif_else.py
@@ -7,8 +7,8 @@ family = ["Pooja", "lekha", "smitha"]
 member = input("enter the members name:")
 
 if member in friends:
-    print(f'Hello, {member}')
+    print(f'Hello, friend: {member}')
 elif member in family:
-    print(f'Hello, {member}')
+    print(f'Hello, family: {member}')
 else:
     print('we dont know you')
