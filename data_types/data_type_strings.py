2614542b71e5377d7ab47e2dd336ebe7bdd46822
Geethanjali-Nagendra <geethanjali.nagendra@gmail.com>
2025-06-14 18:42:32 +0200

    Update data_types/data_type_strings.py
    Co-authored-by: Anil Kaiyambally Sudhakar <58003215+AnilSudhakar@users.noreply.github.com>

--- data_types/data_type_strings.py
+++ data_types/data_type_strings.py
@@ -1,7 +1,7 @@
 #data_types_strings
 
 str_with_single_quotes = "Hello!, It's me, Geethanjali."
-str_with_double_quotes = 'He told, "Its an amazing place to be visited".'
+str_with_double_quotes = 'He told, "Its an amazing place to visit".'
 str_with_escape_character = 'She told, \'Weather is good today\'.' #escaping
 #multiline strings - can be used for commenting
 multiline_string = """Hello!,
